6699b4f19a492d68f1992d2a68c881bdd6a2a5f1
cowpatchmedia <cowpatchmedia@gmail.com>
2025-10-28 17:17:14 -0700

    initial commit for exc. 7 and finalize MDN docs.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
 };
+
+console.log(reverseString('hello there'));
+console.log(reverseString('123! abc! Hello, Odinite.'));
 
 // Do not edit below this line
 module.exports = reverseString;

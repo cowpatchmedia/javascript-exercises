1fcd9593b59dd14369f172bcc14f4eea559cf4ee
cowpatchmedia <cowpatchmedia@gmail.com>
2025-10-28 17:32:26 -0700

    initial commit for excercise 8.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...values) {
+    return arr.filter(num => !values.includes(num));
 };
+
+console.log(removeFromArray([1,2,3,4],3,2));
 
 // Do not edit below this line
 module.exports = removeFromArray;

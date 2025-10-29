d53df6206235279929ac7e03f6e7e51434a712b0
cowpatchmedia <cowpatchmedia@gmail.com>
2025-10-28 17:50:31 -0700

    initial commit for excercise 9.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+    let sum = 0;
+    
+    if (
+        typeof begin !== "number" ||
+        typeof end !== "number" ||
 
+        begin < 0 ||
+        end < 0 ||
+
+        !Number.isInteger(begin) ||
+        !Number.isInteger(end)
+
+    ){
+        return "ERROR";
+    };
+
+    if (begin>end) {
+        let temp = begin;
+        begin = end;
+        end = temp; 
+    }
+
+    for (let i = begin; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

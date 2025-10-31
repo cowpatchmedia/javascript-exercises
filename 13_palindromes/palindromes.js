f2b529949a15abddd3ef30256223accad9f0780c
cowpatchmedia <cowpatchmedia@gmail.com>
2025-10-30 18:39:09 -0700

    initial commit for js exercise 13.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    const clean = word.toLowerCase().replace (/[^a-z0-9]/g,"");
+    const reverse = clean.split("").reverse().join("");
+    
+    return clean === reverse;
+}
 
 // Do not edit below this line
 module.exports = palindromes;

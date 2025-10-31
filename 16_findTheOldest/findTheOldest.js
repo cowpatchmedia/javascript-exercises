93333c0f3e653200d44fa2aa9b8bd2531a9e9129
cowpatchmedia <cowpatchmedia@gmail.com>
2025-10-30 19:15:50 -0700

    initial commit for js exercise 16.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const deathYear = person.yearOfDeath || new Date().getFullYear();
+        const age = deathYear - person.yearOfBirth;
 
+        const oldestDeathYear = oldest.yearOfDeath || new Date().getFullYear();
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+
+        return age > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
